35aa1a7a9d9471a636bc4130b60dca65bb0705c8
Jean Felton <jean665@gmail.com>
2025-08-29 13:12:47 +0530

    Update about.tsx

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -3,7 +3,7 @@
     return (
         <div>
             <h1>
-                About 2.2 - to create test PR
+                About 2.2 - to create test PR (29-08-2025)
             </h1>
         </div>
     )
